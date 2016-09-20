4f7f857a9444c1accbfe40c2e527e44f8571065f
Krishna Prasad Anna Ramesh Kumar <krishprasadar@gmail.com>
2016-09-19 22:02:12 -0400

    A feature filled commit! :)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const exp = require('express');
 const bodyParser = require('body-parser');
-
+const mongodb = require('mongodb');
 const app = exp();
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = mongodb.MongoClient;
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/static', exp.static('/public'));
+app.use('/static', exp.static('public'));
 
 var db;
 
@@ -18,11 +18,6 @@ MongoClient.connect("mongodb://krishprasadar:mlabdb@ds033076.mlab.com:33076/kpsa
     })
 });
 
-app.get("/public/js/home.js", (req, res) => {
-    res.sendFile(__dirname + "/public/js/home.js");
-});
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
@@ -30,17 +25,36 @@ app.get('/', (req, res) => {
 app.post('/app/addTodo', (req, res) => {
     db.collection('todos').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
-        console.log('saved to database')
+        if (result.deletedCount == 1) {
+            result.status = 1;
+            console.log("Todo successfully added!")
+        }
+        else {
+            result.status = 0;
+            console.log("Todo could not be added. Try again!");
+
+        }
         res.json(result)
     });
 });
 
 app.post('/app/deleteTodo', (req, res) => {
-    db.collection('todos').deleteOne(req.body, (err, result) => {
-        if (err) return console.log(err)
-        console.log("Todo successfully deleted!")
-        res.json(result)
-        //return success or failure
+    var del = {_id: mongodb.ObjectID(req.body._id)};
+    //Testing delete failures
+    //var del = {_id: mongodb.ObjectID("57e045150dbe5f482d062ef1")};
+
+    db.collection('todos').deleteOne(del, (err, result) => {
+        if (err) return res.status(500).send(err);
+        if (result.deletedCount == 1) {
+            result.status = 1;
+            console.log("Todo successfully deleted!")
+            res.status(200).json(result);
+        }
+        else {
+            result.status = 0;
+            console.log("Todo successfully failed!");
+            res.status(500).send("FAILURE");
+        }
     })
 });
 
@@ -49,6 +63,28 @@ app.get('/app/getTodos', (req, res) => {
         if (err) return console.log(err)
 
         res.json(result)
+    })
+});
+
+app.post('/app/updateTodo', (req, res) => {
+    var query = {
+        _id: mongodb.ObjectID(req.body._id)
+    }
+    var update = {"$set": {"completed": req.body.completed}};
+
+    db.collection('todos').updateOne(query, update, (err, result) => {
+        if (err) return res.status(500).send(err);
+        if (result.modifiedCount == 1) {
+            result.status = 1;
+            console.log("Todo successfully updated")
+            res.status(200).json(result);
+        }
+        else {
+            result.status = 0;
+            console.log("Todo update failed");
+            console.log(result);
+            res.json(result)
+        }
     })
 });
 
--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,13 @@ var angApp = angular.module('angApp', []);
 angApp.controller('mainController', function ($scope, $http) {
     $scope.formData = {};
 
+    $scope.initialize = function () {
+        $.notify.defaults({ className: "success" });
+        $scope.getTodos();
+    };
+
     $scope.getTodos = function () {
+        console.log("Getting Todos");
         $http.get('/app/getTodos')
             .success(function (data) {
                 $scope.todos = data;
@@ -13,27 +19,37 @@ angApp.controller('mainController', function ($scope, $http) {
                 console.log('Error: ' + data);
             });
 
-    }
+    };
 
     $scope.createTodo = function () {
         console.log($scope.formData.name);
+
+        var filteredTodo = $scope.todos.filter(function(todo){return todo.name == $scope.formData.name});
+        if(filteredTodo.length  > 0)
+        {
+            $("#todoInputArea").notify("Todo already exists", { position:"bottom", className: "info"});
+            return;
+        }
+
         var config = {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         };
 
-        var data = $.param({
-            name: $scope.formData.name
+        var todo = $.param({
+            name: $scope.formData.name,
+            completed: false
         })
 
-        var create = $http.post('/app/addTodo', data, config)
+        var create = $http.post('/app/addTodo', todo, config)
             .success(function (data) {
                 $scope.formData = {}; // clear the form so our user is ready to enter another
-
+                $('#submit').notify("Todo created", { position:"right"});
             })
             .error(function (data) {
                 console.log('Error: ' + data);
+                $('#submit').notify("Todo creation failed. Please try again", { position:"right", className:"error"  });
             })
         create.then($scope.getTodos());
 
@@ -50,20 +66,50 @@ angApp.controller('mainController', function ($scope, $http) {
         };
 
         var data = $.param({
-            _id: todo._id,
-            name: todo.name
+            _id: todo._id
         });
 
-        $http.post('/app/deleteTodo', todo)
+        var del = $http.post('/app/deleteTodo', data, config)
             .success(function (data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                console.log(data);
+                console.log("Delete success " + data);
+                $('#deleteIcon').notify("Todo deleted", { position:"bottom center"});
 
             })
-            .error(function (data) {
-                console.log('Error: ' + data);
+            .error(function (error) {
+                console.log("Error deleting todo. Try again");
+                $.notify("Todo deletion failed. Please try again", { position:"bottom center", className:"error"  });
+            })
+            .catch(function (error) {
+                console.log(error.statusText);
             });
-        $scope.getTodos();
+        del.then($scope.getTodos());
+    };
+
+    //For both false and true
+    $scope.setCompleted = function(isCompleted, todo)
+    {
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        };
+
+        var data = $.param({
+            _id: todo._id,
+            completed: isCompleted
+        });
+
+        var updateTodo = $http.post('/app/updateTodo',data, config)
+            .success(function (data) {
+                console.log(data);
+            })
+            .error(function (error) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.log(error.statusText);
+            });
+        updateTodo.then($scope.getTodos());
     }
 
 });
